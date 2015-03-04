1c7da605113e7257d328909fd3f3bf4ef669480d
Sebastian Kessler <sebastian@herrkessler.de>
2015-03-04 08:55:24 +0100

    add https to gravatar

--- models/user.rb
+++ models/user.rb
@@ -21,7 +21,7 @@ class User
   before :save, :gravatar
 
   def gravatar
-    self.gravatarURL = gravatar_url(self, :size => 200, :default => 'retro')
+    self.gravatarURL = gravatar_url(self, :size => 200, :default => 'retro', :secure => true)
   end
 
   def authenticate(attempted_password)
