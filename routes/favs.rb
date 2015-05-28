53810feaa2261c41c6d498a69f370df7b4a65487
Sebastian Kessler <sebastian@herrkessler.de>
2015-05-28 10:16:38 +0200

    Refactor no-session favs

--- routes/favs.rb
+++ routes/favs.rb
@@ -4,9 +4,15 @@ class Lennep < Sinatra::Base
   # -----------------------------------------------------------
 
    get '/favourites' do
-      @sessionUser = env['warden'].user
+    @sessionUser = env['warden'].user
+    unless @sessionUser == nil
       @favs = @sessionUser.favourites
       slim :"favourites/index"
+    else
+      reqVenues = params[:favs].split(",").map { |s| s.to_i }
+      @favs = Venue.all(:id => reqVenues)
+      slim :"favourites/nosession-index"
+    end
    end
 
 end
